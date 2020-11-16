1b934d6b3f6eeba3a0a1484db636233853c8475c
Kradsat <65266823+Kradsat@users.noreply.github.com>
2020-11-16 16:51:37 +0900

    Add files via upload

---
+++ Scripts/GameLogic.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+
+    [SerializeField] private Dice dice;
+    [SerializeField] private List<Step> playerStep = new List<Step>();
+    [SerializeField] private int turn;
+    [SerializeField] private int playerNumber;
+    [SerializeField] public bool SetDiceRoll = false;
+
+    private void Start()
+    {
+        Init();
+        foreach (var VARIABLE in playerStep)
+        {
+            Debug.Log(VARIABLE.gameObject.name);
+        }
+       
+    }
+
+    public void Init()
+    {
+        turn = 0; // number of turns
+        playerNumber = playerStep.Count; // number of players
+        SetDiceRoll = false;
+    }
+
+    private void turnCheck()
+    {
+        if (playerStep.Count == turn)
+        {
+            turn = 0;
+        }
+    }
+
+    public void SetTurn()
+    {
+        if (!SetDiceRoll)
+        {
+            Debug.Log("======SetTurn======");
+            if (turn + 1 == playerStep.Count)
+            {
+                turn = 0;
+            }
+            else
+            { 
+                turn += 1;
+            }
+
+        }
+        SetDiceRoll = true;
+    }
+
+    public void SetTurn(int TurnNO)
+    {
+        turn += TurnNO;
+    }
+
+    public void DiceRoll()
+    {
+        if (SetDiceRoll)
+        {
+            playerStep[turn].SetStep(dice.GetDiceNumber());
+            turnCheck();
+            SetDiceRoll = false;
+        }
+    }
+}
---
+++ Scripts/Step.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+enum StepStatus
+{
+    stay = 1,
+    Rest = 2,
+}
+public class Step : MonoBehaviour
+{
+    [SerializeField] private int stepValue = 0;
+    [SerializeField] private StepStatus status = StepStatus.stay;
+
+    public void Init()
+    {
+        stepValue = 0;
+        status = StepStatus.stay;
+    }
+
+    public void SetStep(int diceNO)
+    {
+        stepValue += diceNO;
+        Debug.Log(gameObject.name + " Step Value: " + stepValue);
+    }
+}
